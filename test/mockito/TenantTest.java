9fbe781ed083a1414828c8c277f3ea589286a0be
AnithaRamak <anitharamkrishnn@gmail.com>
2023-03-10 18:40:51 -0500

    updating the TenantTest.java
    Creating new Tenant and checking for Not null.

--- test/mockito/TenantTest.java
+++ test/mockito/TenantTest.java
@@ -7,8 +7,25 @@ import org.junit.jupiter.api.Test;
 class TenantTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void testCreateTenant() {
 
+		ArrayList<Object> tenantData = new ArrayList<Object>();
+		tenantData.add("Anitha");
+		tenantData.add("51423425");
+		tenantData.add("ani@gmail.com");
+
+
+		// Create a mock Tenant object
+		Tenant testTenant = Mockito.mock(Tenant.class);
+
+		// Create a Tenant object
+		Tenant tenantObj = new Tenant();
+
+		// Call the create an Tenant method with the mock Tenant object
+		Tenant newTenant = tenantObj.create(tenantData);
+
+		// Verify that the createTenant method returned the not null object
+		assertNotNull(newTenant);
+
+	}
 }
