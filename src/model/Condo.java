9db7f6a80e43ca3eb116ee87ff97fbd746b528f1
AnithaRamak <anitharamkrishnn@gmail.com>
2023-03-13 19:47:42 -0400

    correcting the typo error in name
    correcting the typo error in name

--- src/model/Condo.java
+++ src/model/Condo.java
@@ -7,7 +7,7 @@ import utility.PropertyType;
  * Project Phase 1
  * Student 1: 	Quoc Phong Ngo 				- 40230574
  * Student 2: 	Jimil Suchitkumar Prajapati - 40205477
- * Student 3:   Anitha Ramakrishan			- 40231724
+ * Student 3:   Anitha Ramakrishnan			- 40231724
  * 
  * Condo class
  */
--- src/model/IProperty.java
+++ src/model/IProperty.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Project Phase 1
  * Student 1: 	Quoc Phong Ngo 				- 40230574
  * Student 2: 	Jimil Suchitkumar Prajapati - 40205477
- * Student 3:   Anitha Ramakrishan			- 40231724
+ * Student 3:   Anitha Ramakrishnan			- 40231724
  *
  * IProperty interface
  */
