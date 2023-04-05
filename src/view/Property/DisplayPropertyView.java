6b85740f337d9480e3df1f6e4755430b2becdce6
abc <prajapatijimil@yahoo.com>
2023-04-05 16:38:03 -0400

    scroll feature and resize the window

--- src/view/Property/DisplayPropertyView.java
+++ src/view/Property/DisplayPropertyView.java
@@ -7,6 +7,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -17,7 +18,6 @@ import model.Property;
 import utility.Constant;
 import view.AppBase;
 
-import java.lang.annotation.Target;
 import java.util.ArrayList;
 
 public class DisplayPropertyView extends Application implements AppBase {
@@ -57,58 +57,43 @@ public class DisplayPropertyView extends Application implements AppBase {
                 hID.setSpacing(10);
 
                 Text txtCivicAddress = new Text(180, 180, "Civic address: " + ((Apartment) properties.get(i)).getCivicAddress());
-                txtCivicAddress.setStyle("-fx-text-fill: red;");
                 hCivicAddress = new HBox(txtCivicAddress);
-                hCivicAddress.setSpacing(10);
 
                 Text txtAptNo = new Text(80, 80, "Apt No: "+((Apartment) properties.get(i)).getAptNo());
-                txtAptNo.setStyle("-fx-text-fill: red;");
                 hAptNo = new HBox(txtAptNo);
-                hAptNo.setSpacing(10);
-            } else if(Constant.CONDO_CLASS_NAME.equals(properties.get(i).getClass().getSimpleName())) {
+            } 
+            else if(Constant.CONDO_CLASS_NAME.equals(properties.get(i).getClass().getSimpleName())) {
                 lblHeader.setText(i+1 + ". " + Constant.CONDO_CLASS_NAME);
                 Text txtStreetNo = new Text(80, 80, "Street No: "+((Condo) properties.get(i)).getStreetNo());
-                txtStreetNo.setStyle("-fx-text-fill: red;");
                 hStreetNo = new HBox(txtStreetNo);
-                hStreetNo.setSpacing(10);
 
                 Text txtUnitNo = new Text(80, 80, "Unit No: "+((Condo) properties.get(i)).getUnitNo());
-                txtUnitNo.setStyle("-fx-text-fill: red;");
                 hUnitNo = new HBox(txtUnitNo);
-                hUnitNo.setSpacing(10);
-            } else if(Constant.HOUSE_CLASS_NAME.equals(properties.get(i).getClass().getSimpleName())) {
+            } 
+            else if(Constant.HOUSE_CLASS_NAME.equals(properties.get(i).getClass().getSimpleName())) {
                 lblHeader.setText(i+1 + ". " + Constant.HOUSE_CLASS_NAME);
                 Text txtStreetNo = new Text(80, 80, "Street No: "+((House) properties.get(i)).getStreetNo());
-                txtStreetNo.setStyle("-fx-text-fill: red;");
                 hStreetNo = new HBox(txtStreetNo);
-                hStreetNo.setSpacing(10);
             }
             hHeader = new HBox(lblHeader);
-            hHeader.setSpacing(10);
 
             Text txtNoBedRoom = new Text(80, 80, "Number of bed room: "+((Property) properties.get(i)).getNumberOfBedRoom());
             hBedRoom = new HBox(txtNoBedRoom);
-            hBedRoom.setSpacing(10);
 
             Text txtNoBathRoom = new Text(80, 80, "Number of bath room: "+((Property) properties.get(i)).getNumberOfBathRoom());
             hBathRoom = new HBox(txtNoBathRoom);
-            hBathRoom.setSpacing(10);
 
             Text txtFootage = new Text(80, 80, "Square Footage: "+((Property) properties.get(i)).getSquareFootage());
             hFootage = new HBox(txtFootage);
-            hFootage.setSpacing(10);
 
             Text txtStreetName = new Text(80, 80, "Street Name: "+((Property) properties.get(i)).getStreetName());
             hStreet = new HBox(txtStreetName);
-            hStreet.setSpacing(10);
 
             Text txtCity = new Text(80, 80, "City: "+((Property) properties.get(i)).getCity());
             hCity = new HBox(txtCity);
-            hCity.setSpacing(10);
 
             Text txtPostalCode = new Text(80, 80, "Postal Code: "+((Property) properties.get(i)).getPostalCode());
             hPostalCode = new HBox(txtPostalCode);
-            hPostalCode.setSpacing(10);
 
             if(Constant.APARTMENT_CLASS_NAME.equals(properties.get(i).getClass().getSimpleName())) {
                 vBoxProperty = new VBox(hHeader, hID, hCivicAddress, hAptNo, hBedRoom, hBathRoom,
@@ -120,11 +105,12 @@ public class DisplayPropertyView extends Application implements AppBase {
                 vBoxProperty = new VBox(hHeader, hID, hStreetNo, hBedRoom, hBathRoom,
                         hFootage, hStreet, hCity, hPostalCode);
             }
-            vBoxProperty.setSpacing(25);
-            vBoxProperty.setPadding(new Insets(35, 12, 15, 20));
+            vBoxProperty.setSpacing(5);
+            vBoxProperty.setPadding(new Insets(10));
             vBoxProperty.setAlignment(Pos.CENTER);
             vBoxArray.add(vBoxProperty);
         }
+
         fpanes.getChildren().addAll(vBoxArray);
         // main menu button
         Pane pMenu = new Pane();
@@ -135,9 +121,9 @@ public class DisplayPropertyView extends Application implements AppBase {
         pMenu.getChildren().addAll(hTotal);
 
         if(target == 1) {
-            BorderPane borderPropertyPane = new BorderPane(pMenu);
-            borderPropertyPane.setTop(fpanes);
-            Scene propertyScene = new Scene(borderPropertyPane, 650, 850);
+            ScrollPane scrollPane = new ScrollPane(fpanes);
+            BorderPane borderPropertyPane = new BorderPane(scrollPane);
+            Scene propertyScene = new Scene(borderPropertyPane, 500, 550);
 
             secondStage.setTitle("All properties");
             secondStage.setScene(propertyScene);
@@ -153,7 +139,6 @@ public class DisplayPropertyView extends Application implements AppBase {
     public void start(Stage primaryStage) {
         ArrayList<Property> properties = propertyController.getProperties();
         if(properties.size() > 0) {
-            Stage secondStage = new Stage();
             Thread t = new Thread(new Runnable() {
                 @Override
                 public void run() {
